a24bbb85a8fa4ef3bf52f285e74f7472cc7c14d4
Vyacheslav1557 <slavabirin1@yandex.ru>
2023-09-14 21:08:38 +0500

    fix: изменяет поведение меню тем

--- src/widgets/header/ui/profile/ui.tsx
+++ src/widgets/header/ui/profile/ui.tsx
@@ -1,69 +1,74 @@
-import {Menu, useMantineColorScheme} from "@mantine/core";
+import {Group, MantineColorScheme, Menu, UnstyledButton, useMantineColorScheme} from "@mantine/core";
 import {
     IconAdjustmentsCheck,
     IconBrush,
     IconCheck,
     IconLogout,
     IconMoon,
-    IconSearch,
     IconSun,
     IconUserCircle
 } from '@tabler/icons-react';
 import styles from "./styles.module.css";
+import {useClickOutside, useDisclosure} from "@mantine/hooks";
 
-const ThemeMenu = () => {
+const Profile = () => {
     const {setColorScheme, colorScheme} = useMantineColorScheme();
+    const [opened, {toggle, close}] = useDisclosure(false);
 
-    const addIconCheck = (cs: string) => {
+    const ref = useClickOutside(() => close());
+
+    const addIconCheck = (cs: MantineColorScheme) => {
         if (cs == colorScheme)
             return <IconCheck className={styles.icon}/>;
         return <></>;
     };
 
-    return (
-        <Menu shadow="md" width={200} trigger="hover" position="left">
+
+    const ThemeMenu = (
+        <Menu shadow="md" width={140} position="left">
             <Menu.Target>
-                <Menu.Item leftSection={<IconBrush className={styles.icon}/>}>
-                    Тема
-                </Menu.Item>
+                <UnstyledButton style={{width: "100%"}} px={12} py={6} className={styles.theme}>
+                    <Group gap={10}>
+                        <IconBrush className={styles.icon}/>
+                        <div style={{fontSize: "14px"}}>Тема</div>
+                    </Group>
+                </UnstyledButton>
             </Menu.Target>
+
             <Menu.Dropdown>
                 <Menu.Item
                     leftSection={<IconSun className={styles.icon}/>}
-                    rightSection={addIconCheck("light")}
-                    onClick={() => setColorScheme('light')}>
+                    onClick={() => setColorScheme("light")}
+                    rightSection={addIconCheck("light")}>
                     Светлая
                 </Menu.Item>
                 <Menu.Item
+                    closeMenuOnClick
                     leftSection={<IconMoon className={styles.icon}/>}
-                    rightSection={addIconCheck("dark")}
-                    onClick={() => setColorScheme('dark')}>
+                    onClick={() => setColorScheme("dark")}
+                    rightSection={addIconCheck("dark")}>
                     Тёмная
                 </Menu.Item>
                 <Menu.Item
+                    closeMenuOnClick
                     leftSection={<IconAdjustmentsCheck className={styles.icon}/>}
-                    rightSection={addIconCheck("auto")}
-                    onClick={() => setColorScheme('auto')}>
+                    onClick={() => setColorScheme("auto")}
+                    rightSection={addIconCheck("auto")}>
                     Системная
                 </Menu.Item>
             </Menu.Dropdown>
         </Menu>
     );
-};
 
-const Profile = () => {
+
     return (
-        <Menu shadow="md" width={200}>
+        <Menu shadow="md" width={183} opened={opened}>
             <Menu.Target>
-                <IconUserCircle size="3.4rem" strokeWidth="1"/>
+                <IconUserCircle size="3.4rem" strokeWidth="1" onClick={toggle}/>
             </Menu.Target>
 
-            <Menu.Dropdown>
-                <ThemeMenu/>
-                <Menu.Item
-                    leftSection={<IconSearch className={styles.icon}/>} rightSection="CTRL+K">
-                    Поиск
-                </Menu.Item>
+            <Menu.Dropdown ref={ref}>
+                {ThemeMenu}
                 <Menu.Divider/>
                 <Menu.Item
                     color="red"
