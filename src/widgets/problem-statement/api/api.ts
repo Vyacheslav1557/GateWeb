1a56f6d295aa610855baddb8d30a673746e1800f
Vyacheslav1557 <slavabirin1@yandex.ru>
2023-09-25 15:24:30 +0500

    refactor: выносит повторяющиеся константы

--- src/widgets/problem-statement/api/api.ts
+++ src/widgets/problem-statement/api/api.ts
@@ -1,21 +1,21 @@
 import axios from "axios";
 import {IContest, IGroup, IProblem} from "./types";
 
+const PROBLEMS_API = process.env["NEXT_PUBLIC_PROBLEMS_API"];
+const config = {
+    headers: {'ngrok-skip-browser-warning': 'skip-browser-warning'}
+};
+
 export const ContestService = {
     async getContestByID(contest_id: number) {
-        const response = await axios.get<IContest>(`${process.env["NEXT_PUBLIC_PROBLEMS_API"]}/contest?contest_id=${contest_id}`,
-            {
-                headers: {'ngrok-skip-browser-warning': 'skip-browser-warning'}
-            });
+        const response = await axios.get<IContest>(`${PROBLEMS_API}/contest?contest_id=${contest_id}`, config);
         return response.data;
     },
 };
 
 export const GroupService = {
     async getGroupByID(group_id: number) {
-        const response = await axios.get<IGroup>(`${process.env["NEXT_PUBLIC_PROBLEMS_API"]}/group?group_id=${group_id}`, {
-            headers: {'ngrok-skip-browser-warning': 'skip-browser-warning'}
-        });
+        const response = await axios.get<IGroup>(`${PROBLEMS_API}/group?group_id=${group_id}`, config);
         return response.data;
     },
 };
@@ -23,10 +23,7 @@ export const GroupService = {
 
 export const ProblemService = {
     async getProblemByID(problem_id: number) {
-        const response = await axios.get<IProblem>(`${process.env["NEXT_PUBLIC_PROBLEMS_API"]}/problem?problem_id=${problem_id}`,
-            {
-                headers: {'ngrok-skip-browser-warning': 'skip-browser-warning'}
-            });
+        const response = await axios.get<IProblem>(`${PROBLEMS_API}/problem?problem_id=${problem_id}`, config);
         return response.data;
     },
 };
